301b5758272464350d59bab7c3f081d16bea3e1f
fahrizdimasqy <1197050037@student.uinsgd.ac.id>
2021-05-10 17:48:11 +0700

    add pattern

--- functional_programming.js
+++ functional_programming.js
@@ -59,3 +59,90 @@ const hitungLuas = hitung(luas)
 const hitungLebar = hitung(lebar)
 console.log(hitungLuas(7))
 console.log(hitungLebar(7))
+
+
+// function composition
+/* Salah satu karakteristik dari functional programming adalah adanya function composition.
+Function composition adalah mengkombinasikan beberapa fungsi menjadi fungsi tunggal.
+*/
+
+const user = [
+  { name: 'Alex', age: 30 },
+  { name: 'Dora', age: 12 },
+  { name: 'Xinux', age: 25 },
+]
+
+const users = [
+ { name: 'Alex', age: 30 },
+ { name: 'Dora', age: 12 },
+ { name: 'Xinux', age: 25 },
+]
+/* Dari data tersebut kita ingin mendapatkan nama-nama user yang usianya dewasa (17+), maka ada
+setidaknya dua step yang kita harus lakukan, pertama kita filter dan yang kedua kita ambil properti
+namanya saja.
+*/
+const getAdult = (users) => users.filter(user => user.age > 17 )
+const getName = (users) => users.map(user => user.name)
+// Jalankan fungsi
+let adultUser = getAdult(users)
+let adultName = getName(adultUser)
+console.log(adultName) // [ 'Alex', 'Xinux']
+// Cara di atas sih oke saja, namun bisa kita sederhanakan menjadi compose function sebagai berikut:
+
+// Currying Function
+/* Fungsi currying adalah teknik untuk mengubah bentuk suatu fungsi dengan beberapa argumen menjadi
+sebuah fungsi anak yang berurutan.
+Fungsi currying akan mengembalikan fungsi baru yang sesuai dengan argumen berikutnya.
+Bentuknya seperti berikut ini:*/
+const tambah = (a, b) => a + b
+tambah(2, 5) // 7
+// currying function
+const tambah = a => b => a + b
+tambah(2)(5) // 7
+
+// simulasi untuk komputasi berat
+const getGreeting = () => {
+ let x = 0
+ for (let i=0; i<1000000000; i++){
+ x += i
+ }
+ return "Selamat pagi"
+}
+const say = (callback, name) => {
+ const greeting = callback()
+ console.log(`${greeting} ${name}`)
+}
+const users = ['Fuad', 'Hasan', 'Ismi', 'Budi', 'Azhar']
+users.map(( user => say(getGreeting, user)))
+/* waktu 6 detik
+Selamat pagi Fuad
+Selamat pagi Hasan
+Selamat pagi Ismi
+Selamat pagi Budi
+Selamat pagi Azhar
+*/
+/*Pada kode di atas, fungsi say dipanggil sebanyak jumlah user atau 6 kali, di mana pada setiap iterasinya
+juga akan menjalankan fungsi getGretting yang kita simulasikan sebagai komputasi berat. Hasilnya
+butuh waktu 6 detik.
+Mari kita ubah kode di atas menjadi bentuk currying.
+ */
+ // simulasi untuk komputasi berat
+const getGreeting = () => {
+ let x = 0
+ for (let i=0; i<1000000000; i++){
+   x += i
+ }
+ return "Selamat pagi"
+}
+// ubah menjadi bentuk currying
+const say = (callback) => {
+ const greeting = callback()
+ return (name) => {
+ console.log(`${greeting} ${name}`)
+ }
+}
+const users = ['Fuad', 'Hasan', 'Ismi', 'Budi', 'Azhar']
+// say(getGreeting)(users[0])
+// say(getGreeting)(users[1])
+const sayGretting = say(getGreeting) // jalankan dulu
+users.map(( user => sayGretting(user))
